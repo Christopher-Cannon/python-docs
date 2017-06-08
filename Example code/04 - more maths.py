8e3c2c798beebbc18bb3aa0597d33fa0adeaee4c
CSCannon <Christopher-Cannon@users.noreply.github.com>
2017-06-08 08:05:17 +0100

    Swap example scripts

--- Example code/04 - more maths.py
+++ Example code/04 - more maths.py
@@ -11,12 +11,9 @@ average = int((apples + oranges + bananas) / 3)
 
 print("Average fruit quantity:", average)
 
-operation = 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
-print("Operation:", operation)
-
-minute = 1
-hour = minute * 60
+hour = 60
 minutes_in_12_weeks = ((hour * 24) * 7) * 12
 
 print("Number of minutes in a 12 week period:", minutes_in_12_weeks)
